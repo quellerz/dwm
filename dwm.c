13855d2437f124690dd7ccf7e32fbd50f404f830
quellerz <152263705+quellerz@users.noreply.github.com>
2026-07-14 02:37:38 +0200

    minor fixes of status text

--- dwm.c
+++ dwm.c
@@ -828,7 +828,7 @@ drawbar(Monitor *m)
 	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
-		drw_text(drw, m->ww - sw - 2 * sp, 0, sw, bh, 0, stext, 0);
+		drw_text(drw, m->ww - tw - 2 * sp, 0, tw, bh, 0, stext, 0);
 	}
 
 	for (c = m->clients; c; c = c->next) {
