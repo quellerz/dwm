cfb6dc25e46fbd8e022b3dd1cc92df54f62e599c
quellerz <152263705+quellerz@users.noreply.github.com>
2026-07-14 03:02:50 +0200

    minor keybind changes

--- config.def.h
+++ config.def.h
@@ -106,7 +106,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	/*{ MODKEY,                       XK_s,      show,           {0} },*/
 	/*{ MODKEY|ShiftMask,             XK_s,      showall,        {0} },*/
-	{ MODKEY,                       XK_h,      hide,           {0} },
+	/*{ MODKEY,                       XK_h,      hide,           {0} },*/
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
