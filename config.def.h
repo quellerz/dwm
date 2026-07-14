7cacb51410c2c1156f26d77187c64e8e16cf9e40
quellerz <152263705+quellerz@users.noreply.github.com>
2026-07-14 02:43:39 +0200

    screenshot bind added

--- config.def.h
+++ config.def.h
@@ -68,6 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *scrnshtcmd[]  = { "flameshot","gui", NULL };
 static const char *upvolume[] = { "amixer", "set", "Master", "5%+", NULL };
 static const char *downvolume[] = { "amixer", "set", "Master", "5%-", NULL };
 static const char *mutevolume[] = { "amixer", "set", "Master", "toggle", NULL };
@@ -130,6 +131,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_s,      spawn,          {.v = scrnshtcmd } },
   { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvolume } },
   { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvolume } },
   { 0,                            XF86XK_AudioMute, spawn, {.v = mutevolume } },
