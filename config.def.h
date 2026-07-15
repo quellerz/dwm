62d3f156f01d5a67476f9dfd82758122d1ba334a
quellerz <152263705+quellerz@users.noreply.github.com>
2026-07-16 00:34:45 +0200

    alpha patch applied

--- config.def.h
+++ config.def.h
@@ -15,6 +15,8 @@ static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static unsigned int baralpha        = 0xd0;
+static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
